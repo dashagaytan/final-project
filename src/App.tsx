78d3bad8cd9191cd6950c4109c4c0261ccd5a072
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2025-12-08 14:02:45 -0600

    init project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { Route, Link, Router } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+
+      </div>
+        
+    </>
+  )
+}
+
+export default App
