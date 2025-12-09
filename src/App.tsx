5b296c08ba20196c060007d119aa307689ccba60
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2025-12-09 08:52:20 -0600

    routs setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-// import { Route, Link, Router } from 'react-router-dom'
+import { Routes, Link, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dream from './pages/Dream'
+import Contact from './pages/Contact'
 import './App.css'
 
 function App() {
 
-
   return (
     <>
+    <Navbar/>
       <div>
-
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/dream' element={<Dream/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
       </div>
         
     </>
