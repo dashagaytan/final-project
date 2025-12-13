fd69670895e43088770059df14fe02d2ad8462f9
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2025-12-12 20:05:39 -0600

    about page setup

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,27 @@
+import { Link } from 'react-router-dom'
+import dashagaytan from '../assets/images/dashagaytan.jpg'
+
 export default function About(){
     return(
         <>
-        <div>
-            <h2>About</h2>
+        <div className='about-container'>
+
+            <div className='about-img'>
+                <img src={dashagaytan}/>
+            </div>
+
+            <div className='about-text'>
+                <h1>My Childhood Dream</h1>
+                <p>Growing up, I always dreamed of becoming an artist and working in
+                    creative spaces like galleries. I loved drawing, painting, and imagining stories through visuals.
+                </p>
+                <p>As I got older, that passion evolved into an interest in digital art and design. Eventually leading me into the world of computer science.</p>
+                <p>Today, I combine creativity and technology by building digital experiences.</p>
+
+                <Link to="/">
+                <button>⬅︎ Home Page</button>
+                </Link>
+            </div>
         </div>
         </>
     )
