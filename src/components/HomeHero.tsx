62dee8ec6df6544195678a12ab467220430b3b1f
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2025-12-12 19:51:53 -0600

    updated the home page

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -1,10 +1,12 @@
 export default function HomeHero() {
     return (
-        <section>
-            <h1>When I grow up, I wish to be....</h1>
-            <p> When I was little, I always dreamed of becoming an artist / gallerist. 
-                Then it slowly transferred to a dream of becoming a digital artist.
-                That dream has led me in the space of digital world through computer science within front end development. </p>
+        <section className="hero-page">
+            <div className="hero-content">
+                <h1>When I grow up, I wish to be....</h1>
+                <p> When I was little, I always dreamed of becoming an artist / gallerist. 
+                    Then it slowly transferred to a dream of becoming a digital artist.
+                    That dream has led me in the space of digital world through computer science within front end development. </p>
+            </div>
         </section>
     )
 }
